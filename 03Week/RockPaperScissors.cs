f6004d28bff0d02b86165d1a32b4df105028dc8f
scottgray1 <smg0144@hotmail.com>
2018-03-01 18:33:28 -0600

    committing file

--- 03Week/RockPaperScissors.cs
+++ 03Week/RockPaperScissors.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 
 public class Program
 {
@@ -10,10 +10,35 @@ public class Program
         string hand2 = Console.ReadLine().ToLower();
         Console.WriteLine(CompareHands(hand1, hand2));
     }
-    
+
     public static string CompareHands(string hand1, string hand2)
     {
-        // Your code here
+        //tie
+        if (hand1 == hand2)
+        {
+            Console.WriteLine("It's a tie!");
+        }
+        //Player 1 Paper
+        else if (hand1 == "Paper" && hand2 == "Rock")
+        {
+            Console.WriteLine("Hand 1 Wins!");
+        }
+        // Player 1 Scissors
+        else if (hand1 == "Scissors" && hand2 == "Paper")
+        {
+            Console.WriteLine("Hand 1 Wins!");
+        }
+        // Player 1 Rock
+        else if (hand1 == "Rock" && hand2 == "Scissors")
+        {
+            Console.WriteLine("Hand 1 Wins!");
+        }
+        //Player 2 Wins
+        else
+        {
+            Console.WriteLine("Hand 2 Wins!");
+        }
+        Console.ReadLine();
         return hand1 + " " + hand2;
     }
 }
